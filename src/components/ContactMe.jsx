fb31b01f6575b47d94efbb9e03238836baa79bc3
nadgire <abhinadgire@hotmail.com>
2025-04-30 12:20:18 +0530

    formik and yup for contact me form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -4,6 +4,8 @@ import { RiContactsBook2Fill } from "react-icons/ri";
 import { FaTelegramPlane } from "react-icons/fa";
 import emailjs from 'emailjs-com';
 import Alert from './Alert';
+import { Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function ContactMe() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,14 +17,27 @@ export default function ContactMe() {
     message: ''
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({ ...prevState, [name]: value }));
-  };
+  const validationSchema = Yup.object().shape({
+    fullname: Yup.string()
+      .required('Full name is required')
+      .matches(/^[a-zA-Z\s]+$/, 'Full name must contain only letters and spaces')
+      .min(3, 'Full name must be at least 3 characters long'),
+    phone: Yup.string()
+      .required('Phone number is required')
+      .matches(/^[0-9]+$/, 'Phone number must be digits only')
+      .min(10, 'Phone number must be 10 digits long')
+      .max(10, 'Phone number must be 10 digits long'),
+    email: Yup.string()
+      .required('Email address is required')
+      .email('Email address is not valid'),
+    message: Yup.string()
+      .required('Message is required')
+      .min(20, 'Message must be at least 20 characters long')
+      .max(200, 'Message must be at most 200 characters long')
+  });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const handleSubmit = async (values) => {
+    console.log(values);
     try {
       const result = await emailjs.sendForm(
         'service_oeeuqig',
@@ -62,71 +77,83 @@ export default function ContactMe() {
           Whether you're looking to learn, create a custom website, or need expert help, I'm here to bring your ideas to life!
         </p>
 
-        <form className="my-8 flex flex-col gap-4" onSubmit={handleSubmit}>
-          <div className="grid">
-            <label htmlFor="fullname" className="text-white">Full name</label>
-            <input
-              id="fullname"
-              name="fullname"
-              value={formData.fullname}
-              onChange={handleChange}
-              placeholder="Your Name"
-              type="text"
-              className="bg-white px-3 py-2 rounded-md"
-              aria-label="Full name"
-            />
-          </div>
+        <Formik initialValues={{ fullname: '', phone: '', email: '', message: '' }} validationSchema={validationSchema} onSubmit={(values) => { handleSubmit(values) }}>
+          {({ handleSubmit, handleChange, handleBlur, values }) => (
+            <form className="my-8 flex flex-col gap-4" onSubmit={handleSubmit}>
+              <div className="grid">
+                <label htmlFor="fullname" className="text-white">Full name</label>
+                <Field
+                  id="fullname"
+                  name="fullname"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="Your Name"
+                  type="text"
+                  className="bg-white px-3 py-2 rounded-md"
+                  aria-label="Full name"
+                />
+              </div>
+              <ErrorMessage name="fullname" component="div" className="error text-red-600" />
 
-          <div className="grid">
-            <label htmlFor="phone" className="text-white">Phone</label>
-            <input
-              id="phone"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-              placeholder="08888888888"
-              type="text"
-              className="bg-white px-3 py-2 rounded-md"
-              aria-label="Phone number"
-            />
-          </div>
 
-          <div className="grid">
-            <label htmlFor="email" className="text-white">Email address</label>
-            <input
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="yourid@email.com"
-              type="email"
-              className="bg-white px-3 py-2 rounded-md"
-              aria-label="Email address"
-            />
-          </div>
+              <div className="grid">
+                <label htmlFor="phone" className="text-white">Phone</label>
+                <Field
+                  id="phone"
+                  name="phone"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="08888888888"
+                  type="text"
+                  className="bg-white px-3 py-2 rounded-md"
+                  aria-label="Phone number"
+                />
+              </div>
+              <ErrorMessage name="phone" component="div" className="error text-red-600" />
 
-          <div className="grid">
-            <label htmlFor="message" className="text-white">Your message</label>
-            <textarea
-              rows="4"
-              maxLength={200}
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              placeholder="Looking for a proficient software developer skilled in React and Next.js for a specific project"
-              className="bg-white px-3 py-2 rounded-md"
-              aria-label="Message"
-            />
-          </div>
+              <div className="grid">
+                <label htmlFor="email" className="text-white">Email address</label>
+                <Field
+                  id="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="yourid@email.com"
+                  type="email"
+                  className="bg-white px-3 py-2 rounded-md"
+                  aria-label="Email address"
+                />
+              </div>
+              <ErrorMessage name="email" component="div" className="error text-red-600" />
 
-          <button
-            className="bg-gradient-to-br relative group/btn from-black to-neutral-700 block w-full text-white rounded-md h-10 font-medium hover:shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset]"
-            type="submit"
-          >
-            Submit <FaTelegramPlane className="inline" />
-          </button>
-        </form>
+              <div className="grid">
+                <label htmlFor="message" className="text-white">Your message</label>
+                <Field
+                  rows="4"
+                  maxLength={200}
+                  id="message"
+                  name="message"
+                  as="textarea"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="Looking for a proficient software developer skilled in React and Next.js for a specific project"
+                  className="bg-white px-3 py-2 rounded-md"
+                  aria-label="Message"
+                />
+              </div>
+              <ErrorMessage name="message" component="div" className="error text-red-600" />
+
+              <button
+                className="bg-gradient-to-br relative group/btn from-black to-neutral-700 block w-full text-white rounded-md h-10 font-medium hover:shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset]"
+                type="submit"
+              >
+                Submit <FaTelegramPlane className="inline" />
+              </button>
+            </form>
+          )}
+
+
+        </Formik>
 
         <Alert isOpen={isModalOpen} onClose={handleCloseModal} message={modalMessage} />
       </div>
